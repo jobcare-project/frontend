5786daa2bc87701124438a1404fbda45e63c2bbf
tuanda1102 <phamanhtuan200420@gmail.com>
2022-10-31 20:57:51 +0700

    refactor: update base

--- src/pages/RecruitmentDetail/RecruitmentDetail.js
+++ src/pages/RecruitmentDetail/RecruitmentDetail.js
@@ -1,11 +1,6 @@
 import React from 'react';
-import styles from './RecruitmentDetail.module.scss';
-import classNames from 'classnames/bind';
-import images from '~/assets/images';
 import { Container } from 'react-bootstrap';
 import BoxHeader from '~/pages/RecruitmentDetail/BoxHeader/BoxHeader';
-import DescriptDetail from './RecruitDetail/DescriptDetail';
-const cx = classNames.bind(styles);
 function RecruitmentDetail() {
     return (
         <Container>
