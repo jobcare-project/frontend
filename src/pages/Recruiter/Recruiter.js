ce69f747bcc7074f37f1ceae011eaeb130289a9f
tuanda1102 <phamanhtuan200420@gmail.com>
2022-12-21 22:57:54 +0700

    merge/ui-tuanpa tuanpa merge layout sidebar

--- src/pages/Recruiter/Recruiter.js
+++ src/pages/Recruiter/Recruiter.js
@@ -6,7 +6,7 @@ import styles from './Recruiter.module.scss';
 const cx = classNames.bind(styles);
 
 function Recruiter() {
-    return <div className={cx('wrapper')}>Code bảng tin ở đây</div>;
+    return <div className={cx('wrapper')}>Code bảng tin ở đây !!!</div>;
 }
 
 export default Recruiter;
